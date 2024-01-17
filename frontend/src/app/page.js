8a592d93b8265757adafdcdb5e29149b9717b0fd
Rishikesh <rishikeshjagadale19@gmail.com>
2024-01-17 14:51:36 +0530

    Initialize Material-UI with Next.js and create Navbar component
    Set up Material-UI in the Next.js project, including the necessary configurations and dependencies. Additionally, implement a basic Navbar component with styling and structure, providing a foundation for navigation within the application.

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navbar from "@/Components/Navbar";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Navbar />
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
